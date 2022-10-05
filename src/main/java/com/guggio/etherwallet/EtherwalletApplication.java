ba5534a50fb9a01f9218c8989f771dc42dc2f462
guggio <sebastian.guggisberg@gmail.com>
2022-10-05 20:18:39 +0200

    Initial commit

---
+++ src/main/java/com/guggio/etherwallet/EtherwalletApplication.java
@@ -0,0 +1,13 @@
+package com.guggio.etherwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtherwalletApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EtherwalletApplication.class, args);
+  }
+
+}
